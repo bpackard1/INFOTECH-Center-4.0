fffdf7d465023faaccef6c1ea73459027097c8cb
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-01-23 14:13:58 -0500

    Add time sleep library

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -6,6 +6,13 @@
 Our Welcome Screen will start our Program letting
 drivers know that the InfoTech center 4.0 OS is  loading
 """
+# Import Libraries Here
+from time import sleep
 
+x=2
 print("\n\nWelcome - InfoTechCenter 4.0")
+sleep(2)
 print("\nInfotechCenter 4.0 OS Loading")
+
+
+
