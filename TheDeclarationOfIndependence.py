2648618b332786ddeff470d1ee4ce0d1ffa91af7
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-01-23 13:49:57 -0500

    Welcome Message Update

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,11 @@
 # Programmer: Brennan Packard
 # Date: 1/20/2023
 # Program: Infotech Center Upgrades
+
+"""
+Our Welcome Screen will start our Program letting
+drivers know that the InfoTech center 4.0 OS is  loading
+"""
+
+print("\n\nWelcome - InfoTechCenter 4.0")
+print("\nInfotechCenter 4.0 OS Loading")
