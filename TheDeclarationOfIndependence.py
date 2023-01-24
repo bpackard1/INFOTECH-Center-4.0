eb618fc5abf0b16bc55bda69e3153e01cb0b3dee
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-01-24 14:09:29 -0500

    Text Update

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -14,5 +14,28 @@ print("\n\nWelcome - InfoTechCenter 4.0")
 sleep(2)
 print("\nInfotechCenter 4.0 OS Loading")
 
+#print("\nInfoTech Center 4.0 OS is Loading
 
 
+import time  # I imported the time library for further use in code.
+import sys  # I imported the system library for further use in code.
+
+print('\n\033[7;34;48m Welcome to TheDeclerationOfIndependence')
+
+x = 0
+a = 0
+
+time.sleep(2)
+print('')
+
+
+while x != 20:
+    x += 1
+    b = ("\033[7;33;40m TheDeclarationOfIndependence" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[7;32;40m Done!')
