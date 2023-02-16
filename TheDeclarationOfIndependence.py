2c0a61c1a0e7d5ecf3ed8923768d446d96d41e47
bpackard1 <123008318+bpackard1@users.noreply.github.com>
2023-02-16 13:47:48 -0500

    Update TheDeclarationOfIndependence.py

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,6 +1,11 @@
 # Programmer: Brennan Packard
 # Date: 1/20/2023
 # Program: Infotech Center Upgrades
+import random
+import time
+import sys
+from time import sleep
+
 
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarters Tank", "Full Tank"]
@@ -35,4 +40,61 @@ def gasLevelAlert():
                    print("Your gas tank is only a quarter full.")
                    sleep(1)
                    print("The closest gas station is" listofGasStations(), "which is", milesToGasStationQuarterTank, "miles away")
-                   elif gasLevelIndicator
+                   elif gasLevelIndicator == "Half Tank"
+                   print("\n***Warning***")
+                   sleep(1)
+                   print("\nYour gas tank is half full, which is enough gas to safely arrive to your destintion")
+                   elif gasLevelIndicator == "ThreeQuarterTank")
+                   print("\nYour gas tank is three quarters full, which is more than enough to safely arrive to your destination")
+                   else:
+                   print("\nYour gas tank is full, Good Job, Vroom Vroom")
+                   
+                   
+                   
+                   #Create Weather Conditions ina  list and choose it randomly
+                   
+                   def weather():
+                   weatherForecast = ["Snow", "Blizzard", "Rain", "Fog", "Windy", "Icy", "Sunshine"]
+                   weatherCondition = random.choice(weatherForecast)
+                   return weatherCondition
+                   
+                   #Variable to call weather() once in our VRS
+                   weatherAler = weather()
+                   
+                   #VRS() to respond to the weather conditions
+                   
+                   def vehicleResponseSystem():
+                   if weatherAlert == "snow':
+                   print("NWS has changed your alarm by 15 minutes because of the weather forecast of", weatherAlert)
+                   print("VRS has been engaged only allowing your vehicle to go 45 MPH")
+                   elif weatherAlert == "Blizzard"
+                   print("NWS has changed your alarm by 30 minutes because of the weather forecast of" weatherAlert)
+                   print("VRS has been engaged only allowing your vehicle to go 35 MPH")
+                   elif weatherAlert == "Fog"
+                   print("NWS is calling for", weatherAlert, "conditions, please drive extra careful")
+                   elif weatherAlert == "Windy"
+                   print("NWS is calling for", weatherAlert, "conditions, please drive extra careful")
+                   elif weatherAlert == "Icy"
+                   print("NWS ha changed your alarm by 60 minutes because of", weatherAlert, "road conditions")
+                   print("VRS has been engaged only allowing your vehicle to go 25 MPH")
+                   else:
+                   print("NWS is calling for", weatherAlert, "drive safely and have a Wonderful Day")
+                   
+                   
+                   
+                   #Call Function here
+                   
+                   if gasLevelIndicator == "Empty"
+                   print("\n***WARNING YOU ARE ON EMPTY***)
+                         sleep(1)
+                         print("Calling Emergency Contact")
+                         else:
+                         print("Nation Weather Service is checking conditions...\n")
+                         sleep(2)
+                         vehicleResponseSystem()
+                         print("\nChecking your vehicle's current gas levels...")
+                         sleep(2)
+                         gasLevelAlert()
+                         
+                         
+                         
