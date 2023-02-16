def09404bd5e4cc31754a2a39191d995df4186cf
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-02-16 14:06:51 -0500

    Editing the Color

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -20,7 +20,9 @@ print("\nInfotechCenter 4.0 OS Loading")
 import time  # I imported the time library for further use in code.
 import sys  # I imported the system library for further use in code.
 
+
 print('\n\033[7;34;48m Welcome to TheDeclerationOfIndependence')
+
 
 x = 0
 a = 0
@@ -31,11 +33,16 @@ print('')
 
 while x != 20:
     x += 1
-    b = ("\033[7;33;40m TheDeclarationOfIndependence" + "." * a)
+
+
+    b = ("\033[12;33;48m TheDeclarationOfIndependence" + "." * a)
+
     a = a + 1
     sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[7;32;40m Done!')
+
+        print('\033[4;32;48m Done!')
+
