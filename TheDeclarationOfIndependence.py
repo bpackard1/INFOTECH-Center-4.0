fb68898f93d761076c29b2f8b83374a148b2af82
bpackard1 <123008318+bpackard1@users.noreply.github.com>
2023-02-17 14:12:14 -0500

    Added Radio Station Shuffle

--- TheDeclarationOfIndependence.py
+++ TheDeclarationOfIndependence.py
@@ -1,3 +1,31 @@
 # Programmer: Brennan Packard
 # Date: 1/20/2023
 # Program: Infotech Center Upgrades
+
+#Import Libraries Here
+import time
+import random
+
+def Stations():
+  stationGenre = ["Hip-Hop", "Pop", "Country", "Latin", "Rock", "Electronic", "Heavy Metal"]
+  return stationGenre
+CurrentStation = station
+
+def radio():
+  if currentStation = "Hip-Hop":
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Pop":
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Country"
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Latin":  
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Rock":  
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Electronic":
+    print("\n You're now listening to", currentStation, "Radio.")
+  elif currentStation == "Heavy Metal":  
+    print("\n You're now listening to", currentStation, "Radio.")
+      
+  else:    
+    print("\n You're now listening to", currentStation, "Radio.")
